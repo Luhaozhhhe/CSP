b2c4f911fe357fd8d0b06ef2bb03d4dbf6822624
Luhaozhhhe <150448469+Luhaozhhhe@users.noreply.github.com>
2024-05-15 15:38:23 +0800

    Add files via upload

---
+++ leetcode/1. 两数之和/solution1.java
@@ -0,0 +1,13 @@
+public class solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]+nums[j]==target &i!=j){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[0];
+
+    }
+}
